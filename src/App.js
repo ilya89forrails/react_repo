c8f2504f18e1cbd24420f3092e79f02f31774a97
ilya <ilya89forrails@gmail.com>
2017-01-17 22:46:21 +0900

    almost ready

--- src/App.js
+++ src/App.js
@@ -14,35 +14,29 @@ class App extends Component{
  constructor(props) {
     super(props);
     this.state = {
-      slideIndex: 1,
+      slideIndex: 0,
     };
     this.plusSlides = this.plusSlides.bind(this);
     this.minusSlides = this.minusSlides.bind(this);
-    this.showSlides = this.showSlides.bind(this);
   }
       
 
  plusSlides(){
- 	this.setState({slideIndex: this.state.slideIndex+1});
- 	this.showSlides();
+ 	if (this.state.slideIndex+1 >SLIDES.length-1){
+    	this.setState({slideIndex: 0});
+    } else {
+		this.setState({slideIndex: this.state.slideIndex+1});
+    }	
 }
 
  minusSlides(){
+ 	if (this.state.slideIndex-1 < 0){
+    	this.setState({slideIndex: SLIDES.length-1});
+    } else {
  	this.setState({slideIndex: this.state.slideIndex-1});
- 	this.showSlides();
+	}
 }
-
- showSlides(){
-
-    if (this.state.slideIndex >SLIDES.length){
-    	this.setState({slideIndex: 1});
-    }
-
-    if (this.state.slideIndex < 1){
-    	this.setState({slideIndex: SLIDES.length});
-    }
-}
-
+   
 render() {
     return (
       <div className="App">
@@ -51,15 +45,20 @@ render() {
         		<div className="mySlides fade">
         			<img src={SLIDES[this.state.slideIndex]} alt="" />
         			{this.state.slideIndex}
+        			{SLIDES.length}
         		</div>
-        		<a className="prev" onClick={this.minusSlides}>&#10094</a>
-        		<a className="next" onClick={this.plusSlides}>&#10095</a>
+        		<a className="prev" onClick={this.minusSlides}>&#10094;</a>
+        		<a className="next" onClick={this.plusSlides}>&#10095;</a>
+        		<div className="dots">
+        			<span className="dot" onClick=""></span>
+        			<span className="dot" onClick=""></span>
+        			<span className="dot" onClick=""></span>
+        		</div>
         	</div>
         </div>
       </div>
     );
   } 
-
 }
 
 
