2265b4d241ba46b8cf7b920a6fda4a1b5b0381a1
ilya <ilya89forrails@gmail.com>
2017-01-26 18:41:16 +0900

    slider_ver2

--- src/App.js
+++ src/App.js
@@ -1,98 +1,111 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 
+/*class App extends Component {
+    render() {
+        return <Slider />;
+    }
+}*/
 
- const SLIDES = [
- 				"img/fjords.jpg",
- 				"img/mountains.jpg",
- 				"img/nature.jpg"
+const SLIDES = [
+               "img/fjords.jpg",
+               "img/mountains.jpg",
+               "img/nature.jpg"
             ]
 
- const DOTS = SLIDES.map((number) =>
-  	<span className="dot" key={number.toString()}></span>
- );
 
 
-class App extends Component{
-	render(){
-		return <Slider />;
-	}
+const App = () => (
+    <Slider slides={[
+        "img/fjords.jpg",
+        "img/mountains.jpg",
+        "img/nature.jpg"
+    ]} />
+);
+
+
+class Slider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slideIndex: 0,
+            numOfSlides: 2,
+            colorActiveSlide: false
+        };
+        this.plusSlides = this.plusSlides.bind(this);
+        this.minusSlides = this.minusSlides.bind(this);
+        this.leftBound = this.leftBound.bind(this);
+        this.rightBound = this.rightBound.bind(this);
+    }
+
+
+    plusSlides() {
+        if (this.state.slideIndex + 1 > SLIDES.length - 1) {
+            this.setState({slideIndex: 0});
+        } else {
+            this.setState({slideIndex: this.state.slideIndex + 1});
+        }
+    }
+
+    minusSlides() {
+        if (this.state.slideIndex - 1 < 0) {
+            this.setState({slideIndex: SLIDES.length - 1});
+        } else {
+            this.setState({slideIndex: this.state.slideIndex - 1});
+        }
+    }
+
+    leftBound(){
+        return this.state.slideIndex;
+    }
+
+    rightBound(){
+        return (this.state.slideIndex + this.state.numOfSlides);
+    }
+
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <div className="slideshow-container">
+                        <div className="mySlides fade">
+                            {this.props.slides.length &&
+                                this.props.slides.slice(this.state.slideIndex, this.state.slideIndex + this.state.numOfSlides).map((slide, i) => (
+                                    <img
+                                        src={slide}
+                                        alt=""
+                                        style={{border: (i === this.state.slideIndex && this.state.colorActiveSlide) ? '10px solid orange' : ''}}/>
+                                ))}
+                        </div>
+                           Текущий слайд {this.state.slideIndex} <br/>
+                           Количество {this.state.numOfSlides} <br/>
+                           Текущий+количество {this.state.slideIndex + this.state.numOfSlides} <br/>
+                        <a className="prev" onClick={this.minusSlides}>&#10094;</a>
+                        <a className="next" onClick={this.plusSlides}>&#10095;</a>
+                        <div className="dots">
+                            {this.props.slides.length &&
+                                this.props.slides.map((slide, i) => (
+                                    <span
+                                        className={`dot${(i === this.state.slideIndex) ? ' active' : ''}`}
+                                        onClick={() => this.setState({slideIndex: i})} />
+                                ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 
-class Slider extends Component{
-
- constructor(props) {
-    super(props);
-    this.state = {
-      slideIndex: 0,
-    };
-    this.plusSlides = this.plusSlides.bind(this);
-    this.minusSlides = this.minusSlides.bind(this);
-    this.makeActive = this.makeActive.bind(this);
-  }
-      
-
- plusSlides(){
- 	if (this.state.slideIndex+1 >SLIDES.length-1){
-    	this.setState({slideIndex: 0});
-    } else {
-		this.setState({slideIndex: this.state.slideIndex+1});
-    }	
+class Dot extends Component {
+    render() {
+        return <span className="dot" onClick=""></span>;
+    }
 }
-
- minusSlides(){
- 	if (this.state.slideIndex-1 < 0){
-    	this.setState({slideIndex: SLIDES.length-1});
-    } else {
- 		this.setState({slideIndex: this.state.slideIndex-1});
-	}
-}
-
- makeActive(){
- 	//this.setState({slideIndex: DOTS});
-  		alert("hi!");
-	}	
-
-   
-render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-        	<div className="slideshow-container">
-        		<div className="mySlides fade">
-        			<img src={SLIDES[this.state.slideIndex]} alt="" />
-        			{this.state.slideIndex}
-        			{SLIDES.length}
-        		</div>
-        		<a className="prev" onClick={this.minusSlides}>&#10094;</a>
-        		<a className="next" onClick={this.plusSlides}>&#10095;</a>
-        		<div className="dots">
-
-        			{DOTS}
-        		</div>
-        	</div>
-        </div>
-      </div>
-    );
-  } 
-}
-
-
-
-class Dot extends Component{
-	render(){
-		return <span className="dot" onClick=""></span>;
-	}
-}
-
-
-
-
-
-
-
 
 
 export default App;
